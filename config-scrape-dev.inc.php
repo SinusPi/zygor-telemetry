d242fb5a4acf12004173b9e5766ec16babe3138d
zygordata <zygordata@zygorguides.com>
2025-12-05 20:26:52 -0500

    initial commit: configs and mock storage

---
+++ config-scrape-dev.inc.php
@@ -0,0 +1,4 @@
+<?php
+ return [
+	"SV_STORAGE_ROOT" => __DIR__."/dev_storage",
+ ];
---
+++ dev_storage/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$SYNC_CFG = [
+    'folder'=>"files",
+    'logfolder'=>"logs",
+    'masterpass'=>"foo"
+];
+// sync also obeys main ^/includes/config.inc.php
+
