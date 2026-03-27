05a60a10afe32b068010169dd4b800268d31834c
Sinus <sinus@sinpi.net>
2026-03-27 01:30:27 +0100

    add loader.inc.php

---
+++ loader.inc.php
@@ -0,0 +1,15 @@
+<?php
+require_once __DIR__ . '/classes/Config.class.php';
+require_once __DIR__ . '/classes/FileTools.class.php';
+require_once __DIR__ . '/classes/Telemetry.class.php';
+require_once __DIR__ . '/classes/TelemetryCrunch.class.php';
+require_once __DIR__ . '/classes/TelemetryDB.class.php';
+require_once __DIR__ . '/classes/TelemetryEndpoint.class.php';
+require_once __DIR__ . '/classes/TelemetryScrape.class.php';
+require_once __DIR__ . '/classes/TelemetryScrapePackagerLog.class.php';
+require_once __DIR__ . '/classes/TelemetryScrapeSVs.class.php';
+require_once __DIR__ . '/classes/TelemetryView.class.php';
+
+require_once __DIR__ . '/includes/shell.class.php';
+require_once __DIR__ . '/includes/VerboseException.class.php';
+require_once __DIR__ . '/includes/zygor.class.inc.php';
